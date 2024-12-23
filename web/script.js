9d61a4a62244742a4c87826cc7e3ad650d62780b
Rick <35244153+KidAperture@users.noreply.github.com>
2024-12-23 17:05:53 +0300

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,24 @@
+// script.js
+import CardGame from './game_logic.js';
+
+const game = new CardGame();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const difficultySelect = prompt("Select difficulty: easy, normal, hard");
+  game.initializeGame(difficultySelect);
+
+  // Render initial hand
+  renderHand();
+});
+
+function renderHand() {
+  const gameBoard = document.getElementById('game-board');
+  gameBoard.innerHTML = '';
+
+  game.hand.forEach((card, index) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.innerText = `${card.value} of ${card.suit}`;
+    gameBoard.appendChild(cardElement);
+  });
+}
