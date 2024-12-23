c0f296f5c5af67e7b21074477abacd660f64b7c7
Rick <35244153+KidAperture@users.noreply.github.com>
2024-12-23 17:06:21 +0300

    Create game_logic.js

---
+++ web/game_logic.js
@@ -0,0 +1,44 @@
+// game_logic.js
+class CardGame {
+  constructor() {
+    this.deck = [];
+    this.hand = [];
+    this.coins = 0;
+    this.score = 0;
+    this.difficulty = 'normal'; // Default difficulty
+  }
+
+  initializeGame(difficulty) {
+    this.difficulty = difficulty;
+    this.resetDeck();
+    this.drawInitialHand();
+    this.updateStats();
+  }
+
+  resetDeck() {
+    this.deck = [];
+    for (let suit of ['hearts', 'diamonds', 'clubs', 'spades']) {
+      for (let value = 1; value <= 13; value++) {
+        this.deck.push({ suit, value });
+      }
+    }
+    this.shuffleDeck();
+  }
+
+  shuffleDeck() {
+    this.deck.sort(() => Math.random() - 0.5);
+  }
+
+  drawInitialHand() {
+    this.hand = this.deck.splice(0, 8); // Draw 8 cards
+  }
+
+  updateStats() {
+    document.getElementById('game-stats').innerHTML = `
+      <p>Coins: ${this.coins}</p>
+      <p>Score: ${this.score}</p>
+    `;
+  }
+}
+
+export default CardGame;
